894133c6eb2813a796074a61e7ef8a33c373dfb4
NagamaniKollati <mani.kollati@gmail.com>
2018-12-11 16:14:31 +0530

    greeting message

---
+++ Greeting.java
@@ -0,0 +1,10 @@
+public class Greeting {
+
+    public static void main(String[] args) {
+
+        String path = System.getProperty("user.dir");
+        System.out.println("Hello NAGAMANI!!!!");
+        System.out.println("Working Directory = " + path);
+
+    }
+}
